3e2279110553a789ec2baedb81abacd6165c3272
Saarthak Srivastava <saarthak2k@gmail.com>
2023-01-22 23:58:21 +0530

    Day 14

---
+++ Day14.js
@@ -0,0 +1,39 @@
+let captcha = new Array();
+
+function createCaptcha() {
+  const activeCaptcha = document.getElementById("captcha");
+  for (q = 0; q < 6; q++) {
+    if (q % 2 == 0) {
+      captcha[q] = String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    } else {
+      captcha[q] = Math.floor(Math.random() * 10 + 0);
+    }
+  }
+  theCaptcha = captcha.join("");
+  activeCaptcha.innerHTML = `${theCaptcha}`;
+  document.getElementById("reCaptcha").value = "";
+}
+
+window.onload = function(){ 
+    const changeBtn = document.querySelector('.restart a');
+    changeBtn.addEventListener('click', createCaptcha);
+};
+
+function validateCaptcha() {
+  const errCaptcha = document.getElementById("errCaptcha");
+  const reCaptcha = document.getElementById("reCaptcha");
+  recaptcha = reCaptcha.value;
+  let validateCaptcha = 0;
+  for (var z = 0; z < 6; z++) {
+    if (recaptcha.charAt(z) != captcha[z]) {
+      validateCaptcha++;
+    }
+  }
+  if (recaptcha == "") {
+    errCaptcha.innerHTML = "Re-Captcha must be filled";
+  } else if (validateCaptcha > 0 || recaptcha.length > 6) {
+    errCaptcha.innerHTML = "Wrong captcha";
+  } else {
+    errCaptcha.innerHTML = "Done";
+  }
+}
